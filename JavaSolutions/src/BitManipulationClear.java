9ac453421a2297f122bfdfb78818a5f490acc205
martandtripathi <martand0007@gmail.com>
2026-08-03 23:17:49 +0530

    Bit manipulation new number using Clear.

---
+++ JavaSolutions/src/BitManipulationClear.java
@@ -0,0 +1,10 @@
+public class BitManipulationClear {
+    public static void main(String[] args) {
+        int n = 5;
+        int pos = 2;
+        int bitmask = 1 << pos;
+        int notbitmask = ~(bitmask);
+        int newNumber = notbitmask & n ;
+        System.out.println(newNumber);
+    }
+}
